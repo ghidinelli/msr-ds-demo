e4dd34717caa8303b09bdb62a63f4568344705e7
ChristoSteyn <christo@bare.bz>
2022-10-26 15:20:37 +0200

    chore: Cleanup directory

--- button/src/Button.vue
+++ button/src/Button.vue
@@ -24,14 +24,6 @@ const props = defineProps({
 @import '../../tokens/variables.scss';
 
 button {
-  padding: var(--vue-spacing-00) var(--vue-spacing-2);
-  border-radius: var(--vue-border-radius-pill);
-  border: 1px solid var(--vue-color-border);
-  font-family: var(--vue-font-family);
-  font-size: var(--vue-font-size-00);
-  font-weight: var(--vue-font-weight-button);
-  letter-spacing: var(--vue-letter-spacing-button);
-  text-transform: uppercase;
 
   &.primary {
     color: var(--vue-color-text-on-primary);
